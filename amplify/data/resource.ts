d4949bdf5b06254c2854400c9f2f15a979815dd5
unknown <lenalaptev13@gmail.com>
2025-06-26 18:11:00 +0900

    modified text

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -26,7 +26,7 @@ const schema = a.schema({
       
   generateTodo: a.generation({
     aiModel: a.ai.model('Claude 3 Haiku'),
-    systemPrompt: 'You generate a Todo, deadline, and some children that is reqired to achieve the Todo based on users request. Deadline must be string in the format of "yyyy-MM-DD".',
+    systemPrompt: 'You are a helpful assistant that generates Todos.',
   })
   .arguments({
     description: a.string(),
